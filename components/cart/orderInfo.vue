b0bcb13c6d25f63de6e00defc322f7a0fc4971ca
kondo-riyo <r1y0.tenten.1126@gmail.com>
2021-08-16 16:12:02 +0900

    orderinfo追加

--- components/cart/orderInfo.vue
+++ components/cart/orderInfo.vue
@@ -15,8 +15,9 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                     メールアドレス
                 </label>
-                <validation-provider name="email" rules="required|email">
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-mail" type="text" placeholder="sample@gmail.com">
+                <validation-provider v-slot="{ errors }" name="email" rules="required|email">
+                    <input v-model="value" name="email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-mail" type="text" placeholder="sample@gmail.com">
+                    <span>{{errors[0]}}</span>
                 </validation-provider>
                 </div>
             </div>
@@ -98,12 +99,12 @@ import { required, email } from 'vee-validate/dist/rules';
 extend('email', email);
 extend('required', {
   ...required,
-  message: '{_field_}は必須項目です。'
+  message: '必須項目です。'
 });
 
 export default Vue.extend({
     data(){
-        return {pay:false}
+        return {pay:false,value:''}
     },
     components: {
         // Field,
