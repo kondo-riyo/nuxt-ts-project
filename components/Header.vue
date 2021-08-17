5591cc55676996b45f2e13950b51e8cc15bcfee8
Chigusa-Sato <chigusa.sato@rakus-partners.co.jp>
2021-08-17 15:42:49 +0900

    ヘッダーデザインの修正

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,16 @@
 <template>
-    <div class="space-x-10 space-y-10 flex-col">
-        <div>Header</div>
-        <div><router-link to="/">Home</router-link></div>
-        <div><router-link to="/Cart">カート</router-link></div>
-        <div><router-link to="/OrderLog">購入履歴</router-link></div>
-    </div>
+  <nav class="space-x-10 space-y-5 w-10">
+    <ul class="m-0 p-0">
+      <li class="block mb-5"><img src="~/assets/img/1.png" /></li>
+      <li class="block mb-3 text-center">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li class="block mb-3 text-center">
+        <router-link to="/Cart">カート</router-link>
+      </li>
+      <li class="block mb-3 text-center">
+        <router-link to="/OrderLog">注文履歴</router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
